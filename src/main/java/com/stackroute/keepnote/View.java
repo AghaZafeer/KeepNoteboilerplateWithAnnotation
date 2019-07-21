3e526a16ee2fa1996093b734bb9d9f348e07d901
AghaZafeer <agha.zafeer@gmail.com>
2019-07-21 10:47:41 +0530

    keepnotewithannotation

---
+++ src/main/java/com/stackroute/keepnote/View.java
@@ -0,0 +1,19 @@
+package com.stackroute.keepnote;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+public class View {
+
+    @Bean
+    public ViewResolver viewResolver()
+    {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
